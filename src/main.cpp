53b5948b4a88a62cb42f471181eebce8e1f3a0eb
OSA <zenit33@ukr.net>
2021-11-24 19:43:40 +0200

    Commented OTA update (Flash memory will not enough for OTA after new functional requirement)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ArduinoOTA.h>
+// #include <ArduinoOTA.h>
 
 #include "credentials.hpp"
 
@@ -8,7 +8,7 @@
 #define DEBUG true
 #define Serial if(DEBUG)Serial
 
-#define ATOMIC_FS_UPDATE
+// #define ATOMIC_FS_UPDATE
 
 // Set GPIOs for OUT_RELAY and PIR Motion Sensor
 #define PIR_SENSOR 0
@@ -24,7 +24,7 @@ boolean start_timer = false;
 
 void serial_init();
 void wifi_init();
-void ota_init();
+// void ota_init();
 void gpio_init();
 
 void IRAM_ATTR detects_movement();
@@ -34,13 +34,13 @@ void check_movement();
 void setup() {
   serial_init();
   wifi_init();
-  ota_init();
+  // ota_init();
   gpio_init();
 }
 
 
 void loop() {
-  ArduinoOTA.handle();
+  // ArduinoOTA.handle();
   check_movement();
 }
 
@@ -73,67 +73,67 @@ void wifi_init() {
 }
 
 
-void ota_init() {
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
+// void ota_init() {
+//   // Port defaults to 8266
+//   // ArduinoOTA.setPort(8266);
 
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
+//   // Hostname defaults to esp8266-[ChipID]
+//   // ArduinoOTA.setHostname("myesp8266");
 
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
+//   // No authentication by default
+//   // ArduinoOTA.setPassword("admin");
 
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+//   // Password can be set with it's md5 value as well
+//   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+//   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
+//   ArduinoOTA.onStart([]() {
+//     String type;
+//     if (ArduinoOTA.getCommand() == U_FLASH) {
+//       type = "sketch";
+//     } else { // U_FS
+//       type = "filesystem";
+//     }
 
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
+//     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+//     Serial.println("Start updating " + type);
+//   });
+//   ArduinoOTA.onEnd([]() {
+//     Serial.println("\nEnd");
+//   });
+//   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+//     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+//   });
+//   ArduinoOTA.onError([](ota_error_t error) {
+//     Serial.printf("Error[%u]: ", error);
+//     if (error == OTA_AUTH_ERROR) {
+//       Serial.println("Auth Failed");
+//     } else if (error == OTA_BEGIN_ERROR) {
+//       Serial.println("Begin Failed");
+//     } else if (error == OTA_CONNECT_ERROR) {
+//       Serial.println("Connect Failed");
+//     } else if (error == OTA_RECEIVE_ERROR) {
+//       Serial.println("Receive Failed");
+//     } else if (error == OTA_END_ERROR) {
+//       Serial.println("End Failed");
+//     }
+//   });
 
-  ArduinoOTA.begin();
-  Serial.printf("OTA Ready, free space available: %.f bytes\n",(float)ESP.getFreeSketchSpace());
-}
+//   ArduinoOTA.begin();
+//   Serial.printf("OTA Ready, free space available: %.f bytes\n",(float)ESP.getFreeSketchSpace());
+// }
 
 
-void gpio_init() {
-  // PIR Motion Sensor mode INPUT_PULLUP
-  pinMode(PIR_SENSOR, INPUT_PULLUP);
-  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
-  attachInterrupt(digitalPinToInterrupt(PIR_SENSOR), detects_movement, RISING);
+// void gpio_init() {
+//   // PIR Motion Sensor mode INPUT_PULLUP
+//   pinMode(PIR_SENSOR, INPUT_PULLUP);
+//   // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
+//   attachInterrupt(digitalPinToInterrupt(PIR_SENSOR), detects_movement, RISING);
 
-  // Set OUT_RELAY to LOW
-  pinMode(OUT_RELAY, OUTPUT);
-  digitalWrite(OUT_RELAY, LOW);
-}
+//   // Set OUT_RELAY to LOW
+//   pinMode(OUT_RELAY, OUTPUT);
+//   digitalWrite(OUT_RELAY, LOW);
+// }
 
 
 void IRAM_ATTR detects_movement() {
